2d7fba05536a5c9207ac273bce867bb3796be16a
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-06-27 01:34:16 -0500

    Added new nav menu for selected projects

--- js/apps_hacks.js
+++ js/apps_hacks.js
@@ -8,6 +8,14 @@ function init(){
   $("#quienes_somos").on("click", redirectQuienes);
   $("#in2teck_main").on("click", redirectHome);
   $("#contacto").on("click", redirectContacto);
+  $(".project").on("click", seleccionaMenu);
+}
+
+function seleccionaMenu(event) {
+  $(".project").removeClass("selected");
+  $(".text_project").removeClass("showing");
+  $("#"+event.currentTarget.id).addClass("selected");
+  $("#texto_"+event.currentTarget.id).addClass("showing");
 }
 
 function redirectHome() {
