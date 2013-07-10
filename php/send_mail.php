8236ff44ca4d5429c3bf1a42ec29fa40489fcd2b
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-07-10 11:08:53 -0500

    Added php mailing feature through mailgun.

--- php/send_mail.php
+++ php/send_mail.php
@@ -1,22 +1,21 @@
 <?php
-include("Mail.php"); 
 
-$recipients = "info@in2teck.com"; 
+  $ch = curl_init();
 
-$headers["From"]    = $_GET["email"]; 
-$headers["To"]      = "info@in2teck.com"; 
-$headers["Subject"] = "Contacto pagina web: " . $_GET["nombre"] . " " . $_GET["email"]; 
+  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  curl_setopt($ch, CURLOPT_USERPWD, 'api:key-1dow9lzqm1rva79c2qz2-e407f2qamk7');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-$body = $_GET["body"]; 
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/in2teck.mailgun.org/messages');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, array('from' => 'in2teck@in2teck.mailgun.org',
+                                             'to' => 'info@in2teck.com',
+                                             'subject' => 'Contacto pagina web: ' . $_GET['nombre'] . ' ' . $_GET['email'],
+                                             'text' => $_GET['body']));
 
-$params["host"] = "smtp.gmail.com"; 
-$params["port"] = "587"; 
-$params["auth"] = true; 
-$params["username"] = "in2teck@gmail.com"; 
-$params["password"] = "In2Teck2013"; 
+  $result = curl_exec($ch);
+  curl_close($ch);
 
-$mail_object =& Mail::factory("smtp", $params); 
-
-$mail_object->send($recipients, $headers, $body); 
+  return $result;
 
 ?>
